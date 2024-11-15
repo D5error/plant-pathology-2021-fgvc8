d561de2c391e728442481ed2a79368e6a5913613
D5error <D5error@163.com>
2024-11-16 01:43:07 +0800

    修复计算问题

--- Plant_dataset.py
+++ Plant_dataset.py
@@ -10,17 +10,18 @@ import torch
 
 
 class Plant_dataset: 
-    def __init__(self, dataset_path, train_or_test, checkpoint_path, transform=None):
+    def __init__(self, dataset_path, train_or_test, checkpoint_path, transform, labels_name):
         # 保存参数
         self.checkpoint_path = checkpoint_path # 模型保存的路径
         self.dataset_path = dataset_path # 数据集的路径
         self.transform = transform # 预处理（数据增强等）
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu' # 如果有NVIDA显卡，转到GPU训练，否则用CPU
+        self.labels_name = labels_name # 标签名称
         print(f"正在使用{self.device}")
 
         # 读取数据集
         csv = pd.read_csv(self.dataset_path + "/" + train_or_test + "/"  + train_or_test + '_label.csv')
-        csv = one_hot_encode(csv) # 独热编码
+        csv = one_hot_encode(csv, labels_name) # 独热编码
 
         # 获取图像路径和标签
         self.images_path, self.labels = [], []
@@ -43,8 +44,7 @@ class Plant_dataset:
         label = torch.tensor(self.labels[idx], dtype=torch.float32)
 
         # 图像预处理
-        if self.transform: 
-            img = self.transform(img)
+        img = self.transform(img)
 
         return img, label
 
@@ -129,26 +129,27 @@ class Plant_dataset:
 
 
 # 定义独热编码函数
-def one_hot_encode(data, column='labels'):
-    # 提取data[column]到datadct，键为类型，值为索引
-    datadct = defaultdict(list)
-    for i, label in enumerate(data[column]):
+def one_hot_encode(data, labels_name, column='labels'):
+    # 提取标签
+    labels = []
+    for label in data[column]:
+        all_label = []
         for category in label.split():
-            datadct[category].append(i)
+            all_label.append(labels_name.index(category))
+        labels.append(all_label)
 
-    # 将字典转换为 numpy 数组
-    datadct = {key: np.array(val) for key, val in datadct.items()}
-    
     # 独热编码
-    new_df = pd.DataFrame(np.zeros((data.shape[0], len(datadct.keys())), dtype=np.int8), columns=datadct.keys())
-    for key, val in datadct.items():
-        new_df.loc[val, key] = 1
+    new_df = pd.DataFrame(np.zeros((data.shape[0], len(labels_name)), dtype=np.int8), columns=labels_name)
+    for x, all_label in enumerate(labels):
+        for y in all_label:
+            new_df.iloc[x, y] = 1
+    print(new_df)
 
     return pd.concat([data, new_df], axis=1)
 
 # 运行
-def start(dataset_path, transform, train_or_test, checkpoint_path, batch_size, num_workers, model, loss_function, optimizer, num_epoch):
-    dataset = Plant_dataset(dataset_path, train_or_test, checkpoint_path, transform) # 加载数据集
+def start(dataset_path, transform, train_or_test, checkpoint_path, batch_size, num_workers, model, loss_function, optimizer, num_epoch, labels_name):
+    dataset = Plant_dataset(dataset_path, train_or_test, checkpoint_path, transform, labels_name) # 加载数据集
     dataLoader = DataLoader(dataset, batch_size, True, num_workers = num_workers)
 
     if train_or_test == "train": # 训练
@@ -158,8 +159,37 @@ def start(dataset_path, transform, train_or_test, checkpoint_path, batch_size, n
         model.load_state_dict(torch.load(checkpoint_path, weights_only=True))
         dataset.test(dataLoader, model, loss_function)
 
+
+# 预测一张图片
+def predict_img(img_path, transform, model, num_classes, model_path, labels_name):
+    # 加载模型
+    model.load_state_dict(torch.load(model_path, weights_only=True))
+    model.eval() # 设置模型为评估模式
+
+    # 加载图片
+    image = Image.open(img_path)
+    img = transform(image).unsqueeze(0) # 预处理，增加batch维度
+
+    # 获取模型输出
+    with torch.no_grad():  # 不计算梯度
+        output = model(img)  
+
+    probabilities = torch.nn.functional.softmax(output, dim=1)[0]
+    
+    for idx, probability in enumerate(probabilities):
+        print(f"图片为{labels_name[idx]}的概率：{probability * 100:.2f}%")
+    image.show()
+
 # 测试bug用
 if __name__ == "__main__":
+    labels_name = [  # 标签
+        "scab", # 癣病
+        "healthy", # 健康
+        "frog_eye_leaf_spot", # 蛙眼叶斑病
+        "rust", # 锈病
+        "complex", # 复杂病（可能是多种病害的组合）
+        "powdery_mildew" # 白粉病
+    ]
     data = pd.read_csv("./plant_dataset/test/test_label.csv")
-    data = one_hot_encode(data)
-    print(data)
+    data = one_hot_encode(data, labels_name)
+    # print(data)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from tkinter import Image
 from torchvision import transforms
 import torch.jit
 from torch import nn
 import torch
-import Plant_dataset
+from Plant_dataset import *
 from models import *
 
 
@@ -20,13 +21,29 @@ if __name__ == "__main__":
     ])
 
     # 参数
-    model = LeNet5(num_classes=6) # 创建模型
+    num_classes = 6 # 类别个数
+    model = LeNet5(num_classes) # 创建模型
     loss_function = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9) # 定义优化器(随机梯度下降法)
     train_or_test = "test" # 训练或者测试
     num_epoch = 3 # 训练轮数
     num_workers = 6 # 加载数据时的并行度
     batch_size = 32 # 批量大小
+    labels_name = [  # 标签
+        "scab", # 癣病
+        "healthy", # 健康
+        "frog_eye_leaf_spot", # 蛙眼叶斑病
+        "rust", # 锈病
+        "complex", # 复杂病（可能是多种病害的组合）
+        "powdery_mildew" # 白粉病
+    ]
 
-    # 运行
-    Plant_dataset.start(dataset_path, transform, train_or_test, checkpoint_path, batch_size, num_workers, model, loss_function, optimizer, num_epoch)
+
+##############################################
+    # 开始训练或测试
+    # start(dataset_path, transform, train_or_test, checkpoint_path, batch_size, num_workers, model, loss_function, optimizer, num_epoch, labels_name)
+
+    # 预测一张图片
+    img_path = "./plant_dataset/val/images/8a3937a9ab9265a5.jpg"
+    model_path = "./save_model/checkpoint.pth"
+    predict_img(img_path, transform, model, num_classes, model_path, labels_name)
