41db51c76277098d5ec6466e0ff4249e5aef20d3
D5error <D5error@163.com>
2024-11-15 22:07:48 +0800

    fix bug

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from torch import nn
 import torch
 import Plant_dataset
 from models import *
-from albumentations.pytorch import ToTensorV2
 
 
 if __name__ == "__main__":
@@ -14,14 +13,14 @@ if __name__ == "__main__":
     # 图像预处理
     transform = transforms.Compose([
         transforms.Resize((224, 224)), # 调整图像大小
-        ToTensorV2(), # 将图像转为张量
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0) # 标准化（适用于预训练模型）
+        transforms.ToTensor(), # 将图像转为张量
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # 标准化（适用于预训练模型）
     ])
     model = LeNet5(num_classes=12) # 创建模型
     loss_function = nn.CrossEntropyLoss() # 定义损失函数（交叉熵损失）
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9) # 定义优化器(随机梯度下降法)
-    train_or_test = "test" # 训练或者测试
-    num_epoch = 30 # 轮数
+    train_or_test = "train" # 训练或者测试
+    num_epoch = 100 # 训练轮数
     num_workers = 6 # 加载数据时的并行度
     batch_size = 32 # 批量大小
 
